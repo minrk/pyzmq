d14bfe9499d2f45122452288259e7731b4adb7a7
Min RK <benjaminrk@gmail.com>
2017-08-14 10:54:53 +0200

    try testing on macos

---
+++ ci/before-install.sh
@@ -0,0 +1,44 @@
+set -ex
+env
+if [[ $TRAVIS_OS_NAME == osx ]]; then
+  VENV_PATH=$(mktemp -d)
+  brew install $PYTHON
+  $PYTHON -m pip install --upgrade setuptools pip
+  $PYTHON -m pip install virtualenv
+  $PYTHON -m virtualenv $VENV_PATH
+  export PATH=$VENV_PATH/bin:$PATH
+else
+  sudo add-apt-repository -y ppa:anton+/dnscrypt
+  sudo apt-get update
+fi
+
+if [[ $ZMQ != bundled ]]; then
+  if [[ $TRAVIS_OS_NAME == osx ]]; then
+    brew install zeromq
+  else
+    sudo apt-get install -y -qq libzmq3-dev libsodium-dev
+  fi
+fi
+
+if [[ $TRAVIS_PYTHON_VERSION != pypy* ]]; then
+  pip install -q cython --install-option="--no-cython-compile"
+fi
+
+if [[ ! -z "$ZMQ" && $ZMQ != bundled ]]; then
+  wget https://github.com/zeromq/$ZMQ/archive/master.zip -O libzmq.zip
+  unzip libzmq.zip
+  pushd "$ZMQ-master"
+  ./autogen.sh
+  ./configure
+  make -j
+  sudo make install
+  sudo ldconfig
+  popd
+  export ZMQ=/usr/local
+fi
+
+pip install -r test-requirements.txt
+if [[ "$NOTORNADO" == "1" ]]; then
+  pip uninstall -yq tornado
+fi
+
